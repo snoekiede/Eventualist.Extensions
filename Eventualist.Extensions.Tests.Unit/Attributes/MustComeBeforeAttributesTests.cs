2652b27b51e2c06a8bb21141820074d723d73676
Iede Snoek <info@esoxsolutions.nl>
2023-11-25 16:28:18 +0100

    Added MustComeBeforeAttribute to the package

---
+++ Eventualist.Extensions.Tests.Unit/Attributes/MustComeBeforeAttributesTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Eventualist.Extensions.Attributes;
+using Xunit;
+
+namespace Eventualist.Extensions.Tests.Unit.Attributes
+{
+    internal class TimePeriod
+    {
+        [MustComeBefore("EndDate")]
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+    public class MustComeBeforeAttributesTests
+    {
+        [Fact]
+        public void TestCorrectPeriod()
+        {
+            var period = new TimePeriod
+            {
+                StartDate = DateTime.Now.Subtract(TimeSpan.FromDays(8)),
+                EndDate = DateTime.Now
+            };
+
+            var context = new ValidationContext(period);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(period, context, results, true);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void TestWrongTimePeriod()
+        {
+            var period = new TimePeriod
+            {
+                StartDate = DateTime.Now.Add(TimeSpan.FromDays(8)),
+                EndDate = DateTime.Now
+            };
+
+            var context = new ValidationContext(period);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(period, context, results, true);
+
+            Assert.False(isValid);
+
+        }
+
+        [Fact]
+        public void TestEqualDates()
+        {
+            var now = DateTime.Now;
+            var period = new TimePeriod
+            {
+                StartDate = now,
+                EndDate = now
+            };
+
+            var context = new ValidationContext(period);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(period, context, results, true);
+
+            Assert.False(isValid);
+
+
+        }
+    }
+}
---
+++ Eventualist.Extensions/Attributes/MustComeBeforeAttribute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventualist.Extensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = false)]
+    public class MustComeBeforeAttribute:ValidationAttribute
+    {
+        public string OtherProperty { get; private set; }
+
+
+        public MustComeBeforeAttribute(string otherProperty) : base("Must match")
+        {
+            if (string.IsNullOrEmpty(otherProperty))
+            {
+                throw new ArgumentNullException("otherProperty");
+            }
+
+            this.OtherProperty=otherProperty;
+        }
+
+        public override bool RequiresValidationContext
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            PropertyInfo otherPropertyInfo = validationContext.ObjectType.GetProperty(this.OtherProperty);
+
+            if (otherPropertyInfo == null)
+            {
+                return new ValidationResult($"Unknown property: {this.OtherProperty}");
+            }
+
+            try
+            {
+                DateTime otherPropertyValue =
+                    (DateTime)otherPropertyInfo.GetValue(validationContext.ObjectInstance, null);
+                if (otherPropertyValue == null)
+                {
+                    return new ValidationResult($"{this.OtherProperty} not of type DateTime");
+                }
+
+                var currentDateTime = (DateTime)value;
+                if (currentDateTime < otherPropertyValue)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Dates are in reverse");
+                }
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult($"Exception: {e.Message}");
+            }
+
+        }
+
+    }
+}
